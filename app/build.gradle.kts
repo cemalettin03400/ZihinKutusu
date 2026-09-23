4d9b0e41c5499e090837b27855fff3f248beb3e2
cemalettin03400 <cemalettin_03400@hotmail.com>
2026-09-23 17:07:46 +0300

    Set Java and Kotlin options to version 17
    Updated compile and target compatibility to Java 17 and set JVM target to 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,16 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android { namespace="com.example.zihinkutusu"; compileSdk=35
+android { 
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+}
+namespace="com.example.zihinkutusu"; compileSdk=35
     defaultConfig { applicationId="com.example.zihinkutusu"; minSdk=23; targetSdk=35; versionCode=3; versionName="3.0" }
 }
 dependencies {
